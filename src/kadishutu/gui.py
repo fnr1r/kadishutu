bb75fa85ee3878808a202015bd77353ad9f5a10a
fnrir <stepien241@gmail.com>
2024-08-08 12:25:40 +0200

    Move stack to main window

--- src/kadishutu/gui.py
+++ src/kadishutu/gui.py
@@ -1,11 +1,8 @@
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from enum import Enum, auto
 from pathlib import Path
 import sys
-from typing import Callable, Dict, List, Optional
-from PySide6 import QtWidgets
-import PySide6
-import PySide6.QtCore
+from typing import Dict, List, Optional
 from PySide6.QtGui import QCloseEvent
 #from PySide6.QtWidgets import (
 #    QApplication, QComboBox, QFileDialog, QHBoxLayout, QLabel, QLineEdit,
@@ -16,6 +13,9 @@ from PySide6.QtWidgets import *
 from .demons import STATS_NAMES, HealableEditor, StatsEditor
 from .file_handling import DecryptedSave
 from .game import SaveEditor
+
+
+MAIN_WINDOW: "MainWindow"
 
 
 class QModifiedMixin:
@@ -54,22 +54,14 @@ class EditorMixin:
     def mixin(self):
         parent = self.game_save_editor
         self.save = parent.save
-        self.stack_add = parent.stack_add
-        self.stack_remove = parent.stack_remove
+        self.stack_add = MAIN_WINDOW.stack_add
+        self.stack_remove = MAIN_WINDOW.stack_remove
 
     @property
     def game_save_editor(self) -> "GameSaveEditor":
-        assert hasattr(self, "parentWidget")
-        widget = self.parentWidget()
-        for _ in range(4):
-            if isinstance(widget, GameSaveEditor):
-                return widget
-            widget = widget.parentWidget()
-        raise Exception
-
-    @property
-    def stack_widget(self) -> QStackedWidget:
-        return self.game_save_editor.inner
+        widget = MAIN_WINDOW.inner.widget_stack[1]
+        assert isinstance(widget, GameSaveEditor)
+        return widget
 
     def dispatch(self, cls):
         widget = cls(self)
@@ -184,9 +176,25 @@ class DemonSelectorScreen(GWidget):
         )
 
 
-class GameMainScreen(GWidget, AppliableWidget):
-    def __init__(self, *args, **kwargs):
+class GameSaveEditor(QWidget, AppliableWidget):
+    path: Path
+    raw_save: DecryptedSave
+    save: SaveEditor
+    modified: bool
+
+    def __init__(
+        self,
+        path: Path,
+        raw_save: DecryptedSave,
+        *args,
+        **kwargs
+    ):
         super().__init__(*args, **kwargs)
+        self.path = path
+        self.raw_save = raw_save
+        self.save = SaveEditor(raw_save)
+        self.modified = False
+
         self.setLayout(QVBoxLayout())
         self.macca = QU32(self)
         self.macca.setValue(self.save.macca)
@@ -200,107 +208,12 @@ class GameMainScreen(GWidget, AppliableWidget):
 
     def open_demons_menu(self):
         widget = DemonSelectorScreen(self)
-        self.stack_add(widget)
+        MAIN_WINDOW.stack_add(widget)
 
     def apply_changes(self):
         if self.macca.getModified():
             self.save.macca = self.macca.value()
             self.macca.setModified(False)
-
-
-class GameSaveEditor(QMainWindow):
-    path: Path
-    raw_save: DecryptedSave
-    save: SaveEditor
-    modified: bool
-    widget_stack: List[QWidget]
-
-    def __init__(
-        self,
-        path: Path,
-        raw_save: DecryptedSave,
-        close_callback,
-        *args,
-        **kwargs
-    ):
-        super().__init__(*args, **kwargs)
-        self.path = path
-        self.raw_save = raw_save
-        self.save = SaveEditor(raw_save)
-        self.close_callback = close_callback
-        self.modified = False
-        self.widget_stack = []
-        #self.resize(1280, 720)
-        self.file_menu = QMenu("File", self)
-        self.save_button = self.file_menu.addAction("Save")
-        self.save_button.triggered.connect(self.on_save)
-        self.save_as_button = self.file_menu.addAction("Save as")
-        self.save_as_button.triggered.connect(self.on_save_as)
-        self.menuBar().addMenu(self.file_menu)
-        self.menu_separator = self.menuBar().addSeparator()
-        self.apply_button = self.menuBar().addAction("Apply")
-        self.apply_button.triggered.connect(self.on_apply)
-        self.back_button = self.menuBar().addAction("Back")
-        self.back_button.triggered.connect(self.stack_remove)
-        self.inner = QStackedWidget(self)
-        self.setCentralWidget(self.inner)
-        self.stack_add(GameMainScreen(self.inner))
-        self.back_button.setEnabled(False)
-
-    def on_save(self):
-        self.raw_save.encrypt().save(self.path)
-
-    def on_save_as(self):
-        pass
-
-    def on_apply(self):
-        widget = self.widget_stack[-1]
-        if not isinstance(widget, AppliableWidget):
-            raise NotImplementedError
-        self.modified = True
-        widget.apply_changes()
-
-    def refresh_apply_action(self, widget: QWidget):
-        if isinstance(widget, AppliableWidget):
-            self.apply_button.setEnabled(True)
-        else:
-            self.apply_button.setEnabled(False)
-
-    def stack_add(self, widget: QWidget):
-        widget.setParent(self.inner)
-        self.widget_stack.append(widget)
-        self.inner.addWidget(widget)
-        self.inner.setCurrentWidget(widget)
-        self.back_button.setEnabled(True)
-        self.refresh_apply_action(widget)
-
-    def stack_remove(self):
-        if len(self.widget_stack) < 2:
-            raise ValueError("Refusing to go back with widget stack at 1")
-        widget = self.widget_stack.pop()
-        self.inner.removeWidget(widget)
-        #self.inner.setCurrentWidget(self.widget_stack[-1])
-        if len(self.widget_stack) < 2:
-            self.back_button.setEnabled(False)
-        self.refresh_apply_action(self.widget_stack[-1])
-
-    #@property
-    #def game_main_screen(self) -> GameMainScreen:
-    #    widget = self.widget_stack[0]
-    #    assert isinstance(widget, GameMainScreen)
-    #    return widget
-
-    def closeEvent(self, event: QCloseEvent) -> None:
-        if self.modified:
-            prompt = QMessageBox.question(
-                self, "", "Discard changes?",
-                QMessageBox.StandardButton.Yes, QMessageBox.StandardButton.No
-            )
-            if prompt == QMessageBox.StandardButton.No.value:
-                event.ignore()
-                return
-        self.close_callback(self)
-        return super().closeEvent(event)
 
 
 class SaveType(Enum):
@@ -372,11 +285,9 @@ class FileSelectorPreview(QWidget):
 
 class FileSelectorMenu(QWidget):
     raw: Optional[DecryptedSave] = None
-    editors: List[QWidget]
 
     def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.editors = []
         self.setLayout(QVBoxLayout())
         self.file_path = FileSelector(self)
         self.file_path.file_path_display.textChanged.connect(self.file_selected)
@@ -387,12 +298,12 @@ class FileSelectorMenu(QWidget):
         self.layout().addWidget(self.file_type)
         self.file_preview = FileSelectorPreview(self)
         self.layout().addWidget(self.file_preview)
-        self.file_edit = QPushButton("Edit", self)
-        self.file_edit.setEnabled(False)
-        self.file_edit.clicked.connect(self.on_file_edit)
-        self.layout().addWidget(self.file_edit)
+        self.file_open = QPushButton("Open", self)
+        self.file_open.setEnabled(False)
+        self.file_open.clicked.connect(self.on_file_open)
+        self.layout().addWidget(self.file_open)
 
-    def on_file_edit(self):
+    def on_file_open(self):
         assert self.raw
         if self.ty == SaveType.SysSave:
             QMessageBox.critical(
@@ -400,17 +311,13 @@ class FileSelectorMenu(QWidget):
                 QMessageBox.StandardButton.Ok
             )
             return
-        editor = GameSaveEditor(self.file_path.path, self.raw, self.editors_cleanup, self)
-        editor.show()
-        self.editors.append(editor)
-
-    def editors_cleanup(self, value: QWidget):
-        self.editors.remove(value)
+        editor = GameSaveEditor(self.file_path.path, self.raw, self)
+        MAIN_WINDOW.stack_add(editor)
 
     def file_selected(self):
         path = self.file_path.path
         self.raw = DecryptedSave.auto_open(path)
-        self.file_edit.setEnabled(True)
+        self.file_open.setEnabled(True)
         raw = self.raw
         if len(raw.data) == 4832:
             self.ty = SaveType.SysSave
@@ -425,20 +332,193 @@ class FileSelectorMenu(QWidget):
         self.file_preview.update(save)
 
 
-class MainWindow(QMainWindow):
-    def __init__(self, path: Optional[Path], *args, **kwargs) -> None:
+class LocationBar(QScrollArea):
+    def __init__(self, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
+        self.setFixedHeight(10)
+        self.inner = QWidget(self)
+        self.setWidget(self.inner)
+        self.inner.setLayout(QHBoxLayout())
+        self.inner.layout().setContentsMargins(0, 0, 0, 0)
+        #self.scroll_bar = QScrollBar(QtCore.Orientation.Horizontal, self.scrolla)
+        #self.scrolla.setHorizontalScrollBar(self.scroll_bar)
+        #self.scrolla.horizontalScrollBar().setEnabled(True)
+        #self.scrolla.setHorizontalScrollBarPolicy(QtCore.ScrollBarPolicy.ScrollBarAlwaysOn)
+        #self.inner.setHorizontalScrollBarPolicy(QtCore.ScrollBarPolicy.ScrollBarAlwaysOn)
+        #self.scroll_area.setWidgetResizable(True)
+        #self.inner.setMaximumSize(9999, self.inner.maximumSize().height())
+        #self.inner = QWidget(self.scrolla)
+        #self.setMaximumHeight(40)
+        for i in range(10):
+            txt = chr(64 + i + 1) * 6
+            b = QPushButton(txt, self.inner)
+            b.setContentsMargins(0, 0, 0, 0)
+            self.inner.layout().addWidget(b)
+        #self.a = QLabel("aaaaa", self)
+        #self.addWidget(self.a)
+
+
+class ManagedStackWidget(QWidget):
+    widget_stack: List[QWidget]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.widget_stack = []
+        self.setLayout(QVBoxLayout())
+        self.layout().setContentsMargins(0, 0, 0, 0)
+        # TODO
+        #self.location_bar = LocationBar(self)
+        #self.layout().addWidget(self.location_bar)
+        self.stack = QStackedWidget(self)
+        self.layout().addWidget(self.stack)
+
+    @property
+    def should_show_navigation(self) -> bool:
+        return len(self.widget_stack) > 1
+
+    def stack_add(self, widget: QWidget):
+        widget.setParent(self)
+        self.widget_stack.append(widget)
+        self.stack.addWidget(widget)
+        self.stack.setCurrentWidget(widget)
+        #if self.should_show_navigation:
+        #    self.location_bar.show()
+
+    def stack_remove(self):
+        if len(self.widget_stack) < 2:
+            raise ValueError("Refusing to go back with widget stack at 1")
+        widget = self.widget_stack.pop()
+        self.stack.addWidget(widget)
+        self.stack.setCurrentWidget(self.widget_stack[-1])
+        #if len(self.widget_stack) < 2:
+        #    self.location_bar.hide()
+
+    @property
+    def top_widget(self) -> QWidget:
+        return self.widget_stack[-1]
+
+    @property
+    def editor_widget(self) -> GameSaveEditor:
+        widget = self.widget_stack[1]
+        assert isinstance(widget, GameSaveEditor)
+        return widget
+
+
+class MainWindow(QMainWindow):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        #self.resize(1280, 720)
+
+        self.file_menu = QMenu("File", self)
+        self.save_button = self.file_menu.addAction("Save")
+        self.save_button.triggered.connect(self.on_save)
+        self.save_as_button = self.file_menu.addAction("Save as")
+        self.save_as_button.triggered.connect(self.on_save_as)
+        self.close_button = self.file_menu.addAction("Close")
+        self.close_button.triggered.connect(self.on_file_close)
+        self.file_menu.addSeparator()
+        self.quit_button = self.file_menu.addAction("Quit")
+        self.quit_button.triggered.connect(self.close)
+        self.menuBar().addMenu(self.file_menu)
+        self.edit_menu = QMenu("Edit", self)
+        self.undo_button = self.edit_menu.addAction("Undo")
+        #self.undo_button.triggered.connect(self.on_save)
+        self.menuBar().addMenu(self.edit_menu)
+        self.menu_separator = self.menuBar().addSeparator()
+        self.apply_button = self.menuBar().addAction("Apply")
+        self.apply_button.triggered.connect(self.on_apply)
+        self.back_button = self.menuBar().addAction("Back")
+        self.back_button.triggered.connect(self.on_back)
+
+        self.inner = ManagedStackWidget(self)
+        self.setCentralWidget(self.inner)
+
         self.file_selector = FileSelectorMenu(self)
-        self.setCentralWidget(self.file_selector)
-        if not path:
+        self.stack_add(self.file_selector)
+        self.back_button.setEnabled(False)
+
+    def stack_add(self, widget: QWidget):
+        self.inner.stack_add(widget)
+        self.refresh_nav_actions()
+
+    def stack_remove(self):
+        self.inner.stack_remove()
+        self.refresh_nav_actions()
+
+    def refresh_nav_actions(self):
+        if len(self.inner.widget_stack) > 2:
+            self.back_button.setEnabled(True)
+        else:
+            self.back_button.setEnabled(False)
+        top_widget = self.inner.top_widget
+        if isinstance(top_widget, AppliableWidget):
+            self.apply_button.setEnabled(True)
+        else:
+            self.apply_button.setEnabled(False)
+        if len(self.inner.widget_stack) > 1:
+            self.save_button.setEnabled(True)
+            self.save_as_button.setEnabled(True)
+            self.close_button.setEnabled(True)
+        else:
+            self.save_button.setEnabled(False)
+            self.save_as_button.setEnabled(False)
+            self.close_button.setEnabled(False)
+        #self.statusBar().showMessage(" > ".join([
+        #    i.__class__.__name__
+        #    for i in self.inner.widget_stack
+        #]))
+
+    def on_back(self):
+        self.stack_remove()
+
+    def on_apply(self):
+        widget = self.inner.top_widget
+        if not isinstance(widget, AppliableWidget):
+            raise NotImplementedError
+        self.inner.editor_widget.modified = True
+        widget.apply_changes()
+
+    def on_save(self):
+        if not self.inner.should_show_navigation:
             return
-        self.file_selector.file_path.path = path
-        self.show()
-        self.file_selector.file_edit.click()
+        widget = self.inner.editor_widget
+        widget.raw_save.encrypt().save(widget.path)
+        self.inner.editor_widget.modified = False
+
+    def on_save_as(self):
+        pass
+
+    def on_file_close(self):
+        if self.alert_abandoning_modifications():
+            return
+        while len(self.inner.widget_stack) > 1:
+            self.stack_remove()
+
+    def alert_abandoning_modifications(self) -> bool:
+        if not (self.inner.should_show_navigation and
+            self.inner.editor_widget.modified):
+            return False
+        prompt = QMessageBox.question(
+            self, "", "Discard changes?",
+            QMessageBox.StandardButton.Yes, QMessageBox.StandardButton.No
+        )
+        if prompt == QMessageBox.StandardButton.No.value:
+            return True
+        return False
+
+    def closeEvent(self, event: QCloseEvent) -> None:
+        if self.alert_abandoning_modifications():
+            event.ignore()
+            return
+        return super().closeEvent(event)
 
 
 def gui_main(args):
     app = QApplication()
-    MAIN_WINDOW = MainWindow(args.file)
+    global MAIN_WINDOW
+    MAIN_WINDOW = MainWindow()
+    if args.file:
+        MAIN_WINDOW.file_selector.file_path.path = args.file
+        MAIN_WINDOW.file_selector.file_open.click()
     MAIN_WINDOW.show()
     sys.exit(app.exec())
