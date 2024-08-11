2cdf925fa2bfcd0f934358fa27cf9da788de7e52
fnrir <stepien241@gmail.com>
2024-08-11 20:10:20 +0200

    Add alignment editor

--- src/kadishutu/gui.py
+++ src/kadishutu/gui.py
@@ -1,4 +1,5 @@
 from abc import abstractmethod
+from dataclasses import dataclass
 from enum import Enum, auto
 from pathlib import Path
 import sys
@@ -10,6 +11,7 @@ from PySide6.QtGui import QCloseEvent
 #)
 from PySide6.QtWidgets import *
 
+from .data.alignment import ALIGNMENT_DATA, AlignmentBit
 from .data.demons import DEMON_ID_MAP, DEMON_NAME_MAP
 from .data.element_icons import Element
 from .data.items import CONSUMABLES_RANGE, KEY_ITEMS_RANGE, RELICS_RANGE_1, RELICS_RANGE_2, Item, items_from
@@ -560,6 +562,80 @@ class ItemEditorScreen(GTabWidget, AppliableWidget):
             tab.apply_changes()
 
 
+@dataclass
+class AlignmentPacked:
+    outer: GScrollArea
+    inner: QWidget
+    layout: QGridLayout
+    i: int
+
+    @classmethod
+    def from_parent(cls, parent: GTabWidget, name: str):
+        outer = GScrollArea(parent)
+        outer.setWidgetResizable(True)
+        inner = QWidget(outer)
+        outer.setWidget(inner)
+        layout = QGridLayout(inner)
+        inner.setLayout(layout)
+        parent.addTab(outer, name)
+        return cls(outer, inner, layout, 0)
+
+    def add_widgets(self, *args: QWidget):
+        for i, widget in enumerate(args):
+            widget.setParent(self.inner)
+            self.layout.addWidget(widget, self.i, i)
+        self.i += 1
+
+
+class AlignmentEditorScreen(GTabWidget, AppliableWidget):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.obj = self.save.alignment
+        self.tabmap: Dict[str, AlignmentPacked] = {}
+        self.checkboxmap: Dict[int, QCheckBox] = {}
+
+        for name in ["Main story", "Side quests"]:
+            nameq = name.lower().replace(" ", "_")
+            self.tabmap[nameq] = AlignmentPacked.from_parent(self, name)
+        
+        self.flat_data: List[Tuple[int, AlignmentBit]] = []
+
+        for byte in ALIGNMENT_DATA:
+            for bit in byte.bits:
+                self.flat_data.append((byte.offset, bit))
+
+        for offset, bit in self.flat_data:
+            packed = list(self.tabmap.values())[int(bit.side_quest is None)]
+            packed.add_widgets(
+                QLabel(f"Offset: 0x{offset:04}"),
+                QLabel("Bit: " + str(bit.bit))
+            )
+            packed.add_widgets(
+                QLabel("Alignment: " + bit.alignment.capitalize()),
+                QLabel("Place: " + bit.place)
+            )
+            for text in bit.text:
+                packed.add_widgets(QLabel(text))
+            box = QCheckBox(packed.inner)
+            packed.layout.addWidget(box, packed.i - 1, 1)
+            self.checkboxmap[offset * 8 + bit.bit] = box
+
+
+    def stack_refresh(self):
+        for byte in ALIGNMENT_DATA:
+            byte_editor = self.obj.at_offset(byte.offset)
+            for bit in byte.bits:
+                box = self.checkboxmap[byte.offset * 8 + bit.bit]
+                box.setChecked(byte_editor.get_flag(bit.bit))
+
+    def apply_changes(self):
+        for byte in ALIGNMENT_DATA:
+            byte_editor = self.obj.at_offset(byte.offset)
+            for bit in byte.bits:
+                box = self.checkboxmap[byte.offset * 8 + bit.bit]
+                byte_editor.set_flag(bit.bit, box.isChecked())
+
+
 class GameSaveEditor(QWidget, AppliableWidget):
     path: Path
     raw_save: DecryptedSave
@@ -593,7 +669,8 @@ class GameSaveEditor(QWidget, AppliableWidget):
             ("DLC", DlcEditorScreen),
             ("Player", PlayerEditorScreen),
             ("Demons", DemonSelectorScreen),
-            ("Items", ItemEditorScreen)
+            ("Items", ItemEditorScreen),
+            ("Alignment", AlignmentEditorScreen)
         ]:
             widget = QPushButton(name, self)
             widget.clicked.connect(self.spawner(cls))
