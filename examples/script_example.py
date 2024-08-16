f9157fdb77feba034d274617eeca93ead31404f6
fnrir <stepien241@gmail.com>
2024-08-16 17:56:17 +0200

    Improve example script

---
+++ examples/script_example.py
@@ -0,0 +1,38 @@
+# flake8: noqa: F402
+# Ignore "module level import not at top of file" error
+
+# SELF-REEXEC HEADER
+# DO NOT EDIT
+from pathlib import Path
+from typing import NoReturn
+
+
+def run_through_kadishutu() -> NoReturn:
+    from argparse import ArgumentParser
+    import os
+    import sys
+    parser = ArgumentParser()
+    parser.add_argument("file", type=Path)
+    args = parser.parse_args()
+    script = sys.argv[0]
+    file: Path = args.file
+    os.execlp("kadishutu", "kadishutu", "run_script", script, str(file))
+
+
+if __name__ == "__main__":
+    run_through_kadishutu()
+# EDIT FROM HERE
+
+
+from kadishutu.file_handling import DecryptedSave
+from kadishutu.game import SaveEditor
+from typing import List
+
+
+def main(path: Path, file: DecryptedSave, _: List[str]):
+    game = SaveEditor(file)
+    print(f"Healing player {game.player.names.first_name}")
+    stats = game.player.stats.current
+    game.player.healable.hp = stats.hp
+    game.player.healable.mp = stats.mp
+    file.encrypt().save(path)
