f76d0483adcede905d7f13313d74a84ce3ab204f
fnrir <stepien241@gmail.com>
2024-08-30 18:18:34 +0200

    Move ExtIO into separate file

---
+++ src/kadishutu/tools/extio.py
@@ -0,0 +1,53 @@
+from abc import ABC
+from dataclasses import dataclass
+from typing import BinaryIO, Generic, TypeVar, Union
+from typing_extensions import Self
+
+
+MAGIC_ENCODING = "ascii"
+ENCODING = "UTF-16"
+
+
+T = TypeVar("T")
+
+
+@dataclass
+class WrapperClass(Generic[T], ABC):
+    _inner: T
+
+    def __getattribute__(self, name: str):
+        if name == "_inner":
+            return super().__getattribute__(name)
+        try:
+            return self._inner.__getattribute__(name)
+        except AttributeError:
+            pass
+        return super().__getattribute__(name)
+
+    @classmethod
+    def from_parent(cls, io: T) -> Self:
+        return cls(io)
+
+
+class ExtIO(BinaryIO, WrapperClass):
+    _inner: BinaryIO
+
+    def skip(self, amount: int):
+        self.seek(self.tell() + amount)
+
+    def assert_magic(
+        self,
+        magic: Union[str, bytes],
+        encoding: str = MAGIC_ENCODING
+    ):
+        if isinstance(magic, str):
+            magic = magic.encode(encoding)
+        assert isinstance(magic, bytes)
+        data = self.read(len(magic))
+        assert data == magic, f"Invalid magic {data} instead of {magic}"
+
+    def read_u32_le(self) -> int:
+        return int.from_bytes(self.read(4), byteorder="little")
+
+    def read_chars(self, amount: int, encoding: str = ENCODING) -> str:
+        return self.read(amount).decode(encoding)
--- src/kadishutu/tools/tbbreader.py
+++ src/kadishutu/tools/tbbreader.py
@@ -1,47 +1,12 @@
-from abc import ABC
 from dataclasses import dataclass
 from pathlib import Path
-from typing import BinaryIO, Generic, List, TypeVar
+from typing import BinaryIO, List
 from typing_extensions import Self
 
+from .extio import ExtIO
 
-MAGIC_ENCODING = "ascii"
-ENCODING = "UTF-16"
+
 UEXP_OFFSET = 0x25
-
-
-T = TypeVar("T")
-
-
-@dataclass
-class WrapperClass(Generic[T], ABC):
-    _inner: T
-
-    def __getattribute__(self, name: str):
-        if name == "_inner":
-            return super().__getattribute__(name)
-        try:
-            return self._inner.__getattribute__(name)
-        except AttributeError:
-            pass
-        return super().__getattribute__(name)
-
-    @classmethod
-    def from_parent(cls, io: T) -> Self:
-        return cls(io)
-
-
-class ExtIO(BinaryIO, WrapperClass):
-    _inner: BinaryIO
-
-    def skip(self, amount: int):
-        self.seek(self.tell() + amount)
-
-    def read_chars(self, amount: int) -> str:
-        return self.read(amount).decode(MAGIC_ENCODING)
-
-    def read_u32_le(self) -> int:
-        return int.from_bytes(self.read(4), byteorder="little")
 
 
 @dataclass
@@ -61,7 +26,7 @@ class Tbl:
     def open(cls, file: ExtIO, offset: int) -> Self:
         if file.tell() != offset:
             file.seek(offset)
-        assert file.read_chars(4) == cls.MAGIC
+        file.assert_magic(cls.MAGIC)
         table_size = file.read_u32_le()
         row_size = int.from_bytes(file.read(4), byteorder="little")
         file.skip(4)
@@ -79,7 +44,7 @@ class Tbcr:
     @classmethod
     def _open(cls, file: ExtIO, start_offset: int = 0) -> Self:
         file.seek(start_offset)
-        assert file.read_chars(4) == cls.MAGIC
+        file.assert_magic(cls.MAGIC)
         header_size = file.read_u32_le()
         table_amount = file.read_u32_le()
         table_offsets = [
