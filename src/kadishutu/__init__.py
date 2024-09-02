c22f365f0b69da6d5199206e4d284a0bb643157c
fnrir <stepien241@gmail.com>
2024-09-02 19:07:30 +0200

    refactor: add 3 common classes to __all__

--- src/kadishutu/__init__.py
+++ src/kadishutu/__init__.py
@@ -1,3 +1,9 @@
+from .core.shared.file_handling import DecryptedSave, EncryptedSave
+from .core.game_save.game import GameSaveEditor
 from .main import main
 
-__all__ = ["main"]
+__all__ = [
+    "DecryptedSave", "EncryptedSave",
+    "GameSaveEditor",
+    "main",
+]
